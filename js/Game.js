ea52f25fd9012332c906320e439855d74b4e469b
swastikk06 <royswastikk06@gmail.com>
2020-03-07 09:49:55 +0530

    added player rank

--- js/Game.js
+++ js/Game.js
@@ -44,7 +44,7 @@ class Game {
     form.hide();
     
     Player.getPlayerInfo();
-    
+    player.getCarsAtEnd();
     if(allPlayers !== undefined){
       background(rgb(198,135,103));
       image(track, 0,-displayHeight*4,displayWidth, displayHeight*5);
@@ -93,6 +93,8 @@ class Game {
 
     if(player.distance > 3860){
       gameState = 2;
+      player.rank+=1;
+      Player.updateCarsAtEnd(player.rank);
     }
    
     drawSprites();
@@ -100,5 +102,8 @@ class Game {
 
   end(){
     console.log("Game Ended");
+    console.log(player.rank);
+    
+    text("playerRank"+player.rank,displayWidth/2,-displayHeight*4 -30);
   }
 }
